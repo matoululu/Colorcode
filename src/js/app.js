2689165fdd067ddff7f5dd0dbc7c9de57b08a5ab
Matthew Marineau <matoululu@users.noreply.github.com>
2023-02-15 08:19:01 -0500

    build files

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-
-import InputField from './source/input-field.js';
-import GameField from './source/game-field.js';
-import PopUp from './source/pop-up.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-
-});
--- src/js/source/utils.js
+++
@@ -1,4 +0,0 @@
-export function dispatchEvent(e, data) {
-  const customEvent = new CustomEvent(e, { detail: data, bubbles: true });
-  window.dispatchEvent(customEvent);
-}
