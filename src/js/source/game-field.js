19a065fa204a6118733f8c9c78a4dcd4ff9dfafa
Matthew Marineau <matoululu@users.noreply.github.com>
2023-02-16 21:22:45 -0500

    Latest!

--- src/js/source/game-field.js
+++ src/js/source/game-field.js
@@ -26,9 +26,6 @@ class GameField extends HTMLElement {
       this.answerArray.push(this.chance.integer({min: 1, max: 6}));
     }
 
-    console.log(this.date);
-    console.log(this.answerArray);
-
     window.addEventListener('input:clicked', e => {
       // Add input to current array and display it in this.current, ensure array has a max size of 6
       if (this.currentArray.length >= 4) this.currentArray.shift();
@@ -76,26 +73,42 @@ class GameField extends HTMLElement {
   }
 
   winHandler() {
-    let correct = 0;
+    let resultsCorrect = [];
+    let resultsWrong = [];
+    let resultArray = [];
     const guessArray = this.currentArray;
     const solutionArray = this.answerArray;
     const currentField = this.querySelector(`[data-field="${this.currentTurn}"]`);
 
     for(let i = 0; i < guessArray.length; i++){
       if(guessArray[i] == solutionArray[i]){
-        correct += 1;
-        currentField.children[i].classList.add('input--correct');
-
-        if(correct === 4) {
-          dispatchEvent('game:win');
-          return;
-        }
-      } else if (solutionArray.indexOf(guessArray[i]) != (-1)) {
-        currentField.children[i].classList.add('input--close');
+        resultsCorrect.push('1');
       } else{
-        currentField.children[i].classList.add('input--wrong');
+        resultsWrong.push('2');
       }
     }
+
+    // add correct and wrong to array
+    resultArray = resultsCorrect.concat(resultsWrong);
+
+    // for each item in array, create span with correct or wrong class and append to current field
+    resultArray.forEach((item, i) => {
+      if (item == '1') {
+        const span = document.createElement('span');
+        span.classList.add('icon', 'icon--correct', `icon--${i}`);
+        currentField.appendChild(span);
+      } else {
+        const span = document.createElement('span');
+        span.classList.add('icon', 'icon--wrong', `icon--${i}`);
+        currentField.appendChild(span);
+      }
+    });
+
+    if(resultsCorrect.length === 4) {
+      dispatchEvent('game:win');
+      return;
+    }
+
 
     if (this.currentTurn == 6) {
       dispatchEvent('game:lose');
