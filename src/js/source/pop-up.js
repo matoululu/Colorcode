2db9aaacc0d3c424bb6f66cbc02f63f24c701b00
Matthew Marineau <matoululu@users.noreply.github.com>
2023-04-10 15:25:37 -0400

    share btn!

--- src/js/source/pop-up.js
+++ src/js/source/pop-up.js
@@ -2,7 +2,7 @@
   PopUp
 ==============================================================================*/
 
-import { dispatchEvent, getSolution } from './utils.js';
+import { dispatchEvent, getSolution, getSaveState } from './utils.js';
 
 class PopUp extends HTMLElement {
   constructor() {
@@ -11,6 +11,8 @@ class PopUp extends HTMLElement {
     this.refresh = this.querySelectorAll('[data-refresh]');
     this.popupScreen = this.querySelectorAll('.popup-screen');
     this.solutions = this.querySelectorAll('[data-solution]');
+    this.shareBtns = this.querySelectorAll('[data-share]');
+    this.tries = this.querySelector('[data-tries]');
     this.solutionArray = getSolution();
 
     this.play.forEach(button => {
@@ -55,6 +57,24 @@ class PopUp extends HTMLElement {
 
     // Loop through solutions and add solution to screen
     if (context === 'win' || context === 'lose') {
+      const session = JSON.parse(getSaveState('lastSession'));
+      const tries = session.currentTurn;
+
+
+      // loop thru share buttons and add event listener
+      this.shareBtns.forEach(button => {
+        button.addEventListener('click', () => {
+          navigator.clipboard.writeText(`I just played Colorcodes and got it in ${tries} turn(s)! Can you beat me? https://matoululu.github.io/colorcodes/`);
+          const spanText = button.querySelector('span');
+          spanText.innerHTML = 'Copied!';
+          setTimeout(() => {
+            spanText.innerHTML = 'Share';
+          }, 2000);
+
+        });
+      });
+
+      this.tries.innerHTML = tries;
       this.solutions.forEach(solutionEl => {
         solutionEl.innerHTML = '';
 
